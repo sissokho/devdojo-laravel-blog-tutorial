0105e7a99a3927f68b4e1365d026d0abe336de51
Mouhamadou Moustapha SISSOKHO <siskomouhamed@gmail.com>
2021-10-16 13:10:32 +0000

    Create app layout

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title', 'Laravel Blog')</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/1.9.6/tailwind.min.css">
+</head>
+
+<body>
+    @yield('content')
+</body>
+
+</html>
