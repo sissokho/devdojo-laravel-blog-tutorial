c76be69f2b3a65dd5e833fe9a261fd2e6255b0ea
Mouhamadou Moustapha SISSOKHO <siskomouhamed@gmail.com>
2021-10-16 14:00:28 +0000

    Create post component

---
+++ app/Http/Livewire/PostCreate.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class PostCreate extends Component
+{
+    public function render()
+    {
+        return view('livewire.post-create')
+            ->extends('layouts.app')
+            ->section('content');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Livewire\PostCreate;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,3 +15,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::view('/', 'home');
+
+Route::get('post/create', PostCreate::class);
