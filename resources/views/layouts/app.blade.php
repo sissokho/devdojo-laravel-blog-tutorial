a6d040776058632bb0d1dc7e25faf5f18ec4442b
Mouhamadou Moustapha SISSOKHO <siskomouhamed@gmail.com>
2021-10-16 13:11:34 +0000

    Add livewire styles and scripts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,10 +7,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Laravel Blog')</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/1.9.6/tailwind.min.css">
+    @livewireStyles
 </head>
 
 <body>
     @yield('content')
+
+    @livewireScripts
 </body>
 
 </html>
