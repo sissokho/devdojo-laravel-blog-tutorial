9e5d5f4aae630bfc6c35425b02a6970f9e443824
Mouhamadou Moustapha SISSOKHO <siskomouhamed@gmail.com>
2021-10-16 13:19:13 +0000

    Create homepage

---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container mx-auto p-5">
+    <h1 class="text-4xl mt-32 text-center tracking-tight leading-10 font-extrabold text-gray-900 sm:text-5xl sm:leading-none md:text-6xl">
+        Welcome to the Blog
+    </h1>
+</div>
+@endsection
